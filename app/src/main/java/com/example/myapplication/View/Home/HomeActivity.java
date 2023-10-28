be167343455937bc8fef5e57395551c28ea2c935
gioimtg2003 <conggioi.pro264@gmail.com>
2023-10-29 01:17:25 +0700

    update style button: google, login

--- app/src/main/java/com/example/myapplication/View/Home/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/View/Home/HomeActivity.java
@@ -2,11 +2,12 @@ package com.example.myapplication.View.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.myapplication.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
