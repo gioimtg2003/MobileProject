6de900ee9c543fc97d7e1e5817cd7b7ca5fbe67f
gioimtg2003 <conggioi.pro264@gmail.com>
2023-11-12 10:03:30 +0700

    Model images cho slide images

---
+++ app/src/main/java/com/example/myapplication/Model/Image.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.Model;
+
+public class Image {
+    private int resourceID;
+
+    public Image(int resourceID) {
+        this.resourceID = resourceID;
+    }
+
+    public int getResourceID() {
+        return resourceID;
+    }
+
+    public void setResourceID(int resourceID) { this.resourceID = resourceID; }
+}
