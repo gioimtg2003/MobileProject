2ad18a3dd14ccca4f344f82daf35aa9a68529911
gioimtg2003 <conggioi.pro264@gmail.com>
2023-11-20 10:14:22 +0700

    Repository cho detail

---
+++ app/src/main/java/com/example/myapplication/Repository/DetailRepository.java
@@ -0,0 +1,27 @@
+package com.example.myapplication.Repository;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.example.myapplication.Database.DatabaseProduct;
+
+public class DetailRepository {
+    private Context context;
+    private DatabaseProduct databaseProduct;
+    public DetailRepository(Application application){
+        this.context = application.getApplicationContext();
+        this.databaseProduct = new DatabaseProduct(context);
+    }
+    public void addCart(int idProduct){
+        databaseProduct.addCart(idProduct);
+        Log.d ("APPDATA", "Add cart product: " + String.valueOf(idProduct));
+    }
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+}
