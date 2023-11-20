12a0dae030bcd00c04f8aae5d11834754b51bfda
gioimtg2003 <conggioi.pro264@gmail.com>
2023-11-20 10:14:06 +0700

    ViewModel cho detail

---
+++ app/src/main/java/com/example/myapplication/ViewModel/DetailViewModel.java
@@ -0,0 +1,34 @@
+package com.example.myapplication.ViewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.myapplication.Model.Cart;
+import com.example.myapplication.Repository.DetailRepository;
+
+import java.util.List;
+
+public class DetailViewModel extends AndroidViewModel {
+    private MutableLiveData<Integer> idProduct = new MutableLiveData<Integer>();
+    private DetailRepository detailRepository;
+    public MutableLiveData<Integer> getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(MutableLiveData<Integer> idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public DetailViewModel(@NonNull Application application) {
+        super(application);
+        idProduct.postValue(0);
+        detailRepository = new DetailRepository(application);
+    }
+
+    public void addCart(){
+        detailRepository.addCart(this.idProduct.getValue());
+    }
+}
