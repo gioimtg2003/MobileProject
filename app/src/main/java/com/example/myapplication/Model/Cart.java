fa9f3e72a925ae8711a5a36cae7d903aa9515f7c
gioimtg2003 <conggioi.pro264@gmail.com>
2023-11-20 14:59:02 +0700

    Model cho giỏ hàng

--- app/src/main/java/com/example/myapplication/Model/Cart.java
+++ app/src/main/java/com/example/myapplication/Model/Cart.java
@@ -1,30 +1,22 @@
 package com.example.myapplication.Model;
 
-public class Cart {
-    private int id;
-    private int idProduct;
+public class Cart  extends Product{
+    private Boolean isChecked;
     private int Quantity;
 
-    public Cart(int id, int idProduct, int quantity) {
-        this.id = id;
-        this.idProduct = idProduct;
-        Quantity = quantity;
+    public Cart(int id, String _id, String name, int price, String imageUrl, int Quantity, Boolean isChecked) {
+        super(id, _id, name, price, imageUrl);
+        this.isChecked = isChecked;
+        this.Quantity = Quantity;
     }
 
-    public int getId() {
-        return id;
+
+    public Boolean getChecked() {
+        return isChecked;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getIdProduct() {
-        return idProduct;
-    }
-
-    public void setIdProduct(int idProduct) {
-        this.idProduct = idProduct;
+    public void setChecked(Boolean checked) {
+        isChecked = checked;
     }
 
     public int getQuantity() {
