a5ebfa9b1f8e4981d60a2155f4095d821bb88996
gioimtg2003 <conggioi.pro264@gmail.com>
2023-11-21 23:34:12 +0700

    Code: Utility format giá tiền + format ngày

---
+++ app/src/main/java/com/example/myapplication/Utility.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Utility {
+    public static String formatPrice(int price){
+        DecimalFormat formatter = new DecimalFormat("#,###");
+        return formatter.format(price)  + " đ";
+    }
+    public static String formatDate(String date){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        try {
+            Date date1 = simpleDateFormat.parse(date);
+            SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("dd/MM/yyyy");
+            return simpleDateFormat1.format(date1);
+        }catch (Exception e){
+            return "Error";
+        }
+
+    }
+}
