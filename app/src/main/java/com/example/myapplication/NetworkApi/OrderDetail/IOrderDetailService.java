7b3b946b4c31f122ff23a6989a44ebc34e108469
gioimtg2003 <conggioi.pro264@gmail.com>
2023-11-25 01:14:39 +0700

    Update Code response API cho chi tiết đơn hàng

--- app/src/main/java/com/example/myapplication/NetworkApi/OrderDetail/IOrderDetailService.java
+++ app/src/main/java/com/example/myapplication/NetworkApi/OrderDetail/IOrderDetailService.java
@@ -5,6 +5,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface IOrderDetailService {
-    @GET("/user/order/:userId")
+    @GET("/api/v1/user/order/{userId}")
     Call<ResponseOrderDetail> getOrderDetail(@Path("userId") String userId);
 }
